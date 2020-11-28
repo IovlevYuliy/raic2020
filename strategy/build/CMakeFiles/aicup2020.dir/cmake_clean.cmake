1b6fc797133e8ddd4ce7e141969e57edff6f4db3
Yuliy Iovlev <iovlev2@gmail.com>
2020-11-28 14:31:34 +0700

    adde default startegy files; added local runner

---
+++ strategy/build/CMakeFiles/aicup2020.dir/cmake_clean.cmake
@@ -0,0 +1,38 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aicup2020.dir/DebugInterface.cpp.o"
+  "CMakeFiles/aicup2020.dir/MyStrategy.cpp.o"
+  "CMakeFiles/aicup2020.dir/Stream.cpp.o"
+  "CMakeFiles/aicup2020.dir/TcpStream.cpp.o"
+  "CMakeFiles/aicup2020.dir/main.cpp.o"
+  "CMakeFiles/aicup2020.dir/model/Action.cpp.o"
+  "CMakeFiles/aicup2020.dir/model/AttackAction.cpp.o"
+  "CMakeFiles/aicup2020.dir/model/AttackProperties.cpp.o"
+  "CMakeFiles/aicup2020.dir/model/AutoAttack.cpp.o"
+  "CMakeFiles/aicup2020.dir/model/BuildAction.cpp.o"
+  "CMakeFiles/aicup2020.dir/model/BuildProperties.cpp.o"
+  "CMakeFiles/aicup2020.dir/model/Camera.cpp.o"
+  "CMakeFiles/aicup2020.dir/model/ClientMessage.cpp.o"
+  "CMakeFiles/aicup2020.dir/model/Color.cpp.o"
+  "CMakeFiles/aicup2020.dir/model/ColoredVertex.cpp.o"
+  "CMakeFiles/aicup2020.dir/model/DebugCommand.cpp.o"
+  "CMakeFiles/aicup2020.dir/model/DebugData.cpp.o"
+  "CMakeFiles/aicup2020.dir/model/DebugState.cpp.o"
+  "CMakeFiles/aicup2020.dir/model/Entity.cpp.o"
+  "CMakeFiles/aicup2020.dir/model/EntityAction.cpp.o"
+  "CMakeFiles/aicup2020.dir/model/EntityProperties.cpp.o"
+  "CMakeFiles/aicup2020.dir/model/MoveAction.cpp.o"
+  "CMakeFiles/aicup2020.dir/model/Player.cpp.o"
+  "CMakeFiles/aicup2020.dir/model/PlayerView.cpp.o"
+  "CMakeFiles/aicup2020.dir/model/RepairAction.cpp.o"
+  "CMakeFiles/aicup2020.dir/model/RepairProperties.cpp.o"
+  "CMakeFiles/aicup2020.dir/model/ServerMessage.cpp.o"
+  "CMakeFiles/aicup2020.dir/model/Vec2Float.cpp.o"
+  "CMakeFiles/aicup2020.dir/model/Vec2Int.cpp.o"
+  "aicup2020"
+  "aicup2020.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aicup2020.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
